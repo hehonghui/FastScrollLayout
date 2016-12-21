a5fe05fca4f23f64d7d6c8d1fa340b59e42ed5c1
mr.simple <bboyfeiyu@gmail.com>
2016-12-21 18:04:51 +0800

    添加没有fast scroll 滚动的效果

--- app/src/main/java/com/simple/superscrollview/NoFastScrollActivity.java
+++ app/src/main/java/com/simple/superscrollview/NoFastScrollActivity.java
@@ -1,36 +1,30 @@
 package com.simple.superscrollview;
 
 import android.app.ListActivity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import com.simple.superscrollview.data.Country;
 import com.simple.superscrollview.data.CountryHelper;
 
 import java.util.List;
 
-public class MainActivity extends ListActivity {
+/**
+ * 没有快速滚动效果的页面
+ */
+public class NoFastScrollActivity extends ListActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detail);
+        setTitle("No Fast Scroll");
         
         List<Country> countries = CountryHelper.readInCountries(this);
         
         ArrayAdapter<Country> adapter = new ArrayAdapter<>(
                 this, android.R.layout.simple_spinner_item, countries);
-        ListView listView = (ListView) findViewById(android.R.id.list);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                startActivity(new Intent(MainActivity.this,NoFastScrollActivity.class));
-            }
-        });
+        
         setListAdapter(adapter);
     }
 }
